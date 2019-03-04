9d9cfd8b2c772fe33b16deb5b34f7d6f294475d3
Keisha Ding <keisha.seow@outlook.com>
2019-03-03 19:52:04 -0700

    refactor JS file to script.js

--- practice.js
+++
@@ -1,9 +0,0 @@
-function addList(value, event) {
-    switch (event.which) {
-        case 13:
-            //add List with Enter key
-            console.log(value);
-            $("addList").val("");
-            break;
-    }
-}
