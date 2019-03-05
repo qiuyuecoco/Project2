cb5db72c3f55a26e1126c9b55c5fb312580e1a4b
Keisha Ding <keisha.seow@outlook.com>
2019-03-04 21:33:21 -0700

    fixed JS to completion

--- script.js
+++ script.js
@@ -1,31 +1,66 @@
+//Add class to Item element
+function addChecked(element){
+    $(element).parent().toggleClass('completed');
+}
+//Delete Items element with class 'completed'
+function deleteChecked(){
+    $(".completed").fadeOut(2500);
+    $(".completed").remove();
+}
+//Add Item content into span with delete icon & checkbox
+function addItem(itemID, element) {
+    let itemInput = $(`#${itemID}`).val();
+    // console.log(itemInput);
+    if (itemInput != "") {
+        $(element).siblings(".item").append(
+            `<div class="itemContainer">
+            <input id="itemCheckbox" type="checkbox" class="markCompleted" onchange="addChecked(this)">
+            <i onclick='deleteItem(this)' class='fas fa-trash'></i>
+            <span contenteditable="true"> ${itemInput} </span>
+        </div>`
+        );
+        // $(".item").append
+        // (`
+        // <div class="itemContainer">
+        //     <input id="itemCheckbox" type="checkbox" class="markCompleted">
+        //     <i onclick='deleteItem(this)' class='fas fa-trash'></i>
+        //     <span contenteditable="true"> ${itemInput} </span>
+        // </div>`);
+        $(".itemInput").val("");
+    }
+    // $(".itemInput").focus();
+}
+//add Item with Enter Key
+function itemCheck(event, itemId, element) {
+    switch (event.which) {
+        case 13:
+            addItem(itemId, element);
+            break;
+    }
+}
 //Add List w/ Item Input
-function addList(event) {
+let inputItemID = 0;
+function addList() {
     let listInput = $(".listInput").val();
     if (listInput != "") {
         $("#list").append
             (`<div class="listContainer">
                 <div class='listMain'>
-                    <input id="listCheckbox" type="checkbox" class="markCompleted" onchange="addChecked(this)">
+                    <input id="listCheckbox" type="checkbox" class="markCompleted">
                     <i onclick = 'deleteList(this)' class='fas fa-trash-alt'></i>
                     <span contenteditable='true'> ${listInput} </span>
                 </div>
                 <div id="itemMain">
-                    <input type="text" class="itemInput" onkeyup="itemCheck(event)"/>
-                    <button onclick="addItem(this)">Add Item</button>
+                    <input type="text" id="${inputItemID}" class="itemInput" onkeyup="itemCheck(event, ${inputItemID}, this)"/>
+                    <button onclick="addItem(${inputItemID}, this)">Add Item</button>
                     <div class="item"></div>
                 </div>
             </div>            
             `);
+        inputItemID++;
         $(".listInput").val("");
     }
     $(".listInput").focus();
-}
-
-function addChecked(element){
-    $(element).parent().toggleClass('completed');
-}
-function deleteChecked(){
-    $(".completed").remove();
 }
 
 //add List with Enter Key
@@ -60,34 +95,3 @@ function deleteItem(element) {
 // }
 
 
-//Add Item content into span with delete icon & checkbox
-function addItem(element) {
-    let itemInput = $(".itemInput").val();
-    console.log(itemInput);
-    if (itemInput != "") {
-        $(element).siblings(".item").append(
-            `<div class="itemContainer">
-            <input id="itemCheckbox" type="checkbox" class="markCompleted" onchange="addChecked(this)">
-            <i onclick='deleteItem(this)' class='fas fa-trash'></i>
-            <span contenteditable="true"> ${itemInput} </span>
-        </div>`
-        );
-        // $(".item").append
-        // (`
-        // <div class="itemContainer">
-        //     <input id="itemCheckbox" type="checkbox" class="markCompleted">
-        //     <i onclick='deleteItem(this)' class='fas fa-trash'></i>
-        //     <span contenteditable="true"> ${itemInput} </span>
-        // </div>`);
-        $(".itemInput").val("");
-    }
-    // $(".itemInput").focus();
-}
-//add Item with Enter Key
-function itemCheck(event) {
-    switch (event.which) {
-        case 13:
-            addItem();
-            break;
-    }
-}
