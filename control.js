6d5bb6ca3c93c16f62c0306e4d19887bebf02cb7
Keisha Ding <keisha.seow@outlook.com>
2019-03-09 17:33:44 -0700

    initial commit prior to adding classes

---
+++ control.js
@@ -0,0 +1,19 @@
+// let lists = new Collection();
+//
+// function Collection() {
+//     this.collection = [];
+//     this.add = function (list) {
+//         this.collection.push(new List(list));
+//     };
+//
+// }
+// function List(list) {
+//     this.name = list;
+//     this.collection = [];
+//     this.add = function (task) {
+//         this.collection.push(new Task(task));
+//     };
+// }
+// function Task(name) {
+//     this.name = name;
+// }
--- script.js
+++ script.js
@@ -1,13 +1,56 @@
-//Add class to Item element
-function addChecked(element){
-    $(element).parent().toggleClass('completed');
+// // Store
+// function setData() {
+//     localStorage.list = ("data", JSON.stringify());
+// }
+// // Retrieve
+// function getData () {
+//     $('')
+// }
+// document.getElementById("result").innerHTML = localStorage.lastname;
+
+
+//
+//Add List w/ Item Input
+let inputItemID = 0;
+function addList() {
+    let listInput = $(".listInput").val();
+    if (listInput != "") {
+        $("#list").append
+        (`<div class="listContainer">
+                <div class='listMain'>
+                    <input id="listCheckbox" type="checkbox" class="markCompleted">
+                    <i onclick = 'deleteList(this)' class='fas fa-trash-alt'></i>
+                    <span contenteditable='true'>List Name: ${listInput} </span>
+                </div>
+                <div id="taskMain">
+                    <div class="taskInput d-flex justify-content-center">
+                        <input type="text" id="${inputItemID}" class="itemInput" onkeyup="itemCheck(event, ${inputItemID}, this)"/>
+                        <button class="taskButton" onclick="addItem(${inputItemID}, this)">Add Task</button>
+                    </div>
+                    <div class="item"></div>
+                </div>
+            </div>            
+            `);
+        inputItemID++;
+        $(".listInput").val("");
+    }
+    $(".listInput").focus();
 }
-//Delete Items element with class 'completed'
-function deleteChecked(){
-    $(".completed").fadeOut("slow", function () {
-        $(".completed").remove();
+//add List with Enter Key
+function listCheck(event) {
+    switch (event.which) {
+        case 13:
+            addList();
+            break;
+    }
+}
+//delete List with trash icon
+function deleteList(element) {
+    $(element).parent().parent().fadeOut("slow", function () {
+        $(element).parent().parent().remove();
     });
 }
+
 //Add Item content into span with delete icon & checkbox
 function addItem(itemID, element) {
     let itemInput = $(`#${itemID}`).val();
@@ -32,47 +75,6 @@ function itemCheck(event, itemId, element) {
             break;
     }
 }
-//Add List w/ Item Input
-let inputItemID = 0;
-function addList() {
-    let listInput = $(".listInput").val();
-    if (listInput != "") {
-        $("#list").append
-            (`<div class="listContainer">
-                <div class='listMain'>
-                    <input id="listCheckbox" type="checkbox" class="markCompleted">
-                    <i onclick = 'deleteList(this)' class='fas fa-trash-alt'></i>
-                    <span contenteditable='true'> ${listInput} </span>
-                </div>
-                <div id="itemMain">
-                    <div class="taskInput d-flex justify-content-center">
-                        <input type="text" id="${inputItemID}" class="itemInput" onkeyup="itemCheck(event, ${inputItemID}, this)"/>
-                        <button class="taskButton" onclick="addItem(${inputItemID}, this)">Add Task</button>
-                    </div>
-                    <div class="item"></div>
-                </div>
-            </div>            
-            `);
-        inputItemID++;
-        $(".listInput").val("");
-    }
-    $(".listInput").focus();
-}
-
-//add List with Enter Key
-function listCheck(event) {
-    switch (event.which) {
-        case 13:
-            addList();
-            break;
-    }
-}
-//delete List with trash icon
-function deleteList(element) {
-    $(element).parent().parent().fadeOut("slow", function () {
-        $(element).parent().parent().remove();
-    });
-}
 //delete Item with icon
 function deleteItem(element) {
     console.log(element);
@@ -81,13 +83,14 @@ function deleteItem(element) {
     });
 }
 
-//hide ITEM until there is an available LIST
-// function callITEM(validITEM) {
-//     for (let i = 0; i < validITEM.length; i++) {
-//         if {
-//             let list${""}
-//         };
-//     }
-// }
-
+//Add class to Item element
+function addChecked(element){
+    $(element).parent().toggleClass('completed');
+}
+//Delete Items element with class 'completed'
+function deleteChecked(){
+    $(".completed").fadeOut("slow", function () {
+        $(".completed").remove();
+    });
+}
 
